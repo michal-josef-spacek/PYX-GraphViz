f1884e79665da3d89f710f573519d1c86dc54546
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-09-11 23:15:42 +0200

    Improve LICENSE AND COPYRIGHT section in doc

--- lib/PYX/GraphViz.pm
+++ lib/PYX/GraphViz.pm
@@ -333,8 +333,9 @@ L<http://skim.cz>
 
 =head1 LICENSE AND COPYRIGHT
 
- © 2011-2019 Michal Josef Špaček
- BSD 2-Clause License
+© 2011-2019 Michal Josef Špaček
+
+BSD 2-Clause License
 
 =head1 VERSION
 
